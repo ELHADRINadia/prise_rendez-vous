2f359d136e13e009f5be59be81d8a32fe0e86d9f
Nadia ELHADRI <elhadrinadia219@gmail.com>
2022-04-05 02:21:44 +0200

    add front end && back end

---
+++ back-end/api/models/User.php
@@ -0,0 +1,81 @@
+<?php
+
+class User
+{
+
+    // private $conn;  
+    // private $table = 'users';
+    private  $reference;
+    private  $firstname;
+    private  $lastname;
+    private  $age;
+
+    // public function __construct($db)
+    // {
+    //     $this->conn = $db;
+    // }
+
+    public function setReference($reference)
+    {
+        $this->reference = $reference;
+    }
+
+    public function setFirstName($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+    public function setLastName($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+
+    public function count()
+    {
+
+        $qr = "SELECT count(*) as nb from users";
+        $res = DB::connect()->query($qr)->fetch(PDO::FETCH_ASSOC);
+        return $res;
+    }
+
+    public function addUser($tab)  //ajouterU
+    {
+
+        $ref = $tab['reference'];
+        $first = $tab['firstname'];
+        $last = $tab['lastname'];
+        $age = $tab['age'];
+
+        // $qr='INSERT INTO users (reference, firstname, lastname, age) values("'.$this->reference.'","'.$this->firstname.'","'.$this->lastname.'",'.$this->age.'")';
+        $qr = "INSERT INTO `users` ( `reference`, `firstname`, `lastname`, `age`) values  ( '$ref'  ,'$first','$last','$age') ";
+        // die($qr);
+        $res = DB::connect()->prepare($qr);
+        if ($res->execute()) {
+            return 'ok';
+        } else {
+            return 'no';
+        }
+    }
+
+    public function Signin()
+    {
+        $qr='SELECT * from users where reference="'.$this->reference.'"';
+        $res=DB::connect()->query($qr);
+       if( $row=$res->fetch(PDO::FETCH_ASSOC)){
+        return $row;
+        
+       }
+       else{
+        return array("message"=>"la reference entrer est invalid");
+       }
+        
+        //    $res->close();
+           $res=null;
+      
+    }
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div class="navbar navbar-dark bg-dark">
+      <a href="#" class="navbar-brand">Doctors</a>
+      <ul class="nav">
+        <li class="nav-item px-3">
+          <router-link class="text-white" to="/">Home</router-link>
+        </li>
+        <!-- <li class="nav-item px-3">
+          <router-link class="text-white" to="#">mon compte</router-link>
+        </li> -->
+        <li class="nav-item px-3">
+          <router-link class="text-white" to="/Dashbord">Dashboard</router-link>
+        </li>
+        <li class="nav-item px-3">
+          <a href="#" @click.prevent="Logout" style="color: white">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
